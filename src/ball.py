46bc22bd0681ff442aca86b6189b3767b4853f1a
Julián Zorzenón <comm.ass@gmail.com>
2025-04-26 01:58:57 -0300

    Add ball collisions (#15)

--- src/ball.py
+++ src/ball.py
@@ -1,14 +1,26 @@
 from os import path
-from typing import List, Set, Tuple
+from typing import List, Set
 
 import pygame
 
 from src.blackboard import Blackboard
 from src.collision import Collision, ObjectType
-from src.constants import BALL_INITIAL_COORD, STAGE_IMAGES_FOLDER_PATH, CoordPosition
+from src.constants import (
+  BALL_INITIAL_COORD,
+  BALL_VELOCITY,
+  STAGE_IMAGES_FOLDER_PATH,
+  VERTICAL_OFFSET_BETWEEN_BALL_AND_BAT,
+  X_COORD,
+  X_LEFT,
+  X_RIGHT,
+  Y_COORD,
+  Y_DOWN,
+  Y_UP,
+  CoordPosition,
+)
 from src.interfaces import GameObject
 from src.notifications import NotificationType
-from src.utils import create_sprite_from_surface, load_image
+from src.utils import create_sprite_from_surface, follow_shadow, load_image
 
 
 class Ball(GameObject):
@@ -27,6 +39,10 @@ class Ball(GameObject):
       shadow_group=shadow_group
     )
 
+    self._follow_bat = False
+    self._bat_frect = None
+    self._direction = None
+
   def reacts_to_collisions(self) -> bool:
     return True
 
@@ -39,16 +55,72 @@ class Ball(GameObject):
     ]
 
   def has_collided(self, collision: Collision) -> None:
-    pass
+    match collision.object_type:
+      case ObjectType.SIDE_WALL:
+        if collision.rect.center[X_COORD] < self._ball_sprite.rect.center[X_COORD]:
+          # Colliding to the left
+          self._ball_sprite.rect.topleft = (
+            collision.rect.topright[X_COORD],
+            self._ball_sprite.rect.topleft[Y_COORD]
+          )
+
+          y = Y_DOWN if self._direction[Y_COORD] > 0 else Y_UP
+          self._direction = pygame.Vector2(X_RIGHT, y).normalize()
+        else:
+          # Colliding to the right
+          self._ball_sprite.rect.topright = (
+            collision.rect.topleft[X_COORD],
+            self._ball_sprite.rect.topleft[Y_COORD]
+          )
+
+          y = Y_DOWN if self._direction[Y_COORD] > 0 else Y_UP
+          self._direction = pygame.Vector2(X_LEFT, y).normalize()
+      case ObjectType.TOP_WALL:
+        self._ball_sprite.rect.topleft = (
+          self._ball_sprite.rect.topleft[X_COORD],
+          collision.rect.bottomleft[Y_COORD]
+        )
+
+        x = X_RIGHT if self._direction[X_COORD] > 0 else X_LEFT
+        self._direction = pygame.Vector2(x, Y_DOWN).normalize()
+      case ObjectType.BAT:
+        self._ball_sprite.rect.center = (
+          self._ball_sprite.rect.center[X_COORD],
+          collision.rect.center[Y_COORD] - VERTICAL_OFFSET_BETWEEN_BALL_AND_BAT
+        )
+
+        x = X_RIGHT if self._ball_sprite.rect.center[X_COORD] > collision.rect.center[X_COORD] else X_LEFT
+        self._direction = pygame.Vector2(x, Y_UP).normalize()
 
   def get_interested_notification_types(self) -> Set[NotificationType] | None:
-    return None
+    return {
+      NotificationType.BALL_RELEASED
+    }
 
   def emit_notification(self) -> NotificationType | None:
     return None
 
-  def receive_notification(self, notification: NotificationType, blackboard: Blackboard) -> None:
-    pass
+  def receive_notification(self, notification_type: NotificationType, blackboard: Blackboard) -> None:
+    match notification_type:
+      case NotificationType.INITIAL_SETUP | NotificationType.BALL_MISSED:
+        self._follow_bat = True
+        self._bat_frect = blackboard.bat_frect
+      case NotificationType.BALL_RELEASED:
+        if self._follow_bat:
+          self._follow_bat = False
+          self._bat_frect = None
+
+          self._direction = pygame.Vector2(X_RIGHT, Y_UP).normalize()
 
   def update(self, delta_ms: float) -> None:
-    pass
+    if self._follow_bat:
+      pos = self._bat_frect.center
+
+      self._ball_sprite.rect.center = (
+        pos[X_COORD],
+        pos[Y_COORD] - VERTICAL_OFFSET_BETWEEN_BALL_AND_BAT
+      )
+    else:
+      self._ball_sprite.rect.center += self._direction * BALL_VELOCITY * delta_ms
+
+    follow_shadow(self._ball_sprite, self._shadow_sprite)
--- src/notifications.py
+++ src/notifications.py
@@ -2,6 +2,13 @@ from enum import Enum
 
 
 class NotificationType(Enum):
+    # When the ball passes by the bat
     BALL_MISSED = 1
-    BRICK_DESTROYED = 2
-    PLAYER_DIED = 3
+    # When the player releases the ball when attached to the bat
+    BALL_RELEASED = 2
+    # When the ball hits a brick
+    BRICK_DESTROYED = 3
+    # When the game first starts
+    INITIAL_SETUP = 4
+    # When the number of lives of the player has reached zero
+    PLAYER_DIED = 5
